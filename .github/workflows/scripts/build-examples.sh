5c95e65e37508b8348d3a4762214b05136d5baf9
Volodymyr Buberenko <v.buberenko@gmail.com>
2021-10-10 17:00:58 +0300

    Modify all_plugins workflow, move android_alarm_manager build into a separate workflow

--- .github/workflows/scripts/build-examples.sh
+++ .github/workflows/scripts/build-examples.sh
@@ -2,14 +2,14 @@
 
 DEFAULT_TARGET="./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart"
 
-ACTION=$1
+JOB_NAME=$1
 TARGET_FILE=${2:-$DEFAULT_TARGET}
 
-melos bootstrap --scope="$PLUS_PLUGIN_SCOPE"
+melos bootstrap --scope="$PLUGIN_SCOPE"
 
-if [ "$ACTION" == "android" ]
+if [ "$JOB_NAME" == "android" ]
 then
-  melos exec -c 1 --scope="$PLUS_PLUGIN_SCOPE_EXAMPLE" -- \
+  melos exec -c 1 --scope="$PLUGIN_EXAMPLE_SCOPE" -- \
     flutter build apk $FLUTTER_COMMAND_FLAGS --debug --target="$TARGET_FILE" --dart-define=CI=true --no-android-gradle-daemon
   MELOS_EXIT_CODE=$?
   pkill dart || true
@@ -17,37 +17,37 @@ then
   exit $MELOS_EXIT_CODE
 fi
 
-if [ "$ACTION" == "ios" ]
+if [ "$JOB_NAME" == "ios" ]
 then
-  melos exec -c 1 --scope="$PLUS_PLUGIN_SCOPE_EXAMPLE" -- \
+  melos exec -c 1 --scope="$PLUGIN_EXAMPLE_SCOPE" -- \
     flutter build ios $FLUTTER_COMMAND_FLAGS --no-codesign --simulator --debug --target="$TARGET_FILE" --dart-define=CI=true
   exit
 fi
 
-if [ "$ACTION" == "macos" ]
+if [ "$JOB_NAME" == "macos" ]
 then
-  melos exec -c 1 --scope="$PLUS_PLUGIN_SCOPE_EXAMPLE" -- \
+  melos exec -c 1 --scope="$PLUGIN_EXAMPLE_SCOPE" -- \
     flutter config --enable-macos-desktop && flutter build macos $FLUTTER_COMMAND_FLAGS --debug --target="$TARGET_FILE" --dart-define=CI=true
   exit
 fi
 
-if [ "$ACTION" == "linux" ]
+if [ "$JOB_NAME" == "linux" ]
 then
-  melos exec -c 1 --scope="$PLUS_PLUGIN_SCOPE_EXAMPLE" -- \
+  melos exec -c 1 --scope="$PLUGIN_EXAMPLE_SCOPE" -- \
     flutter config --enable-linux-desktop && flutter build linux $FLUTTER_COMMAND_FLAGS --debug --target="$TARGET_FILE" --dart-define=CI=true
   exit
 fi
 
-if [ "$ACTION" == "windows" ]
+if [ "$JOB_NAME" == "windows" ]
 then
-  melos exec -c 1 --scope="$PLUS_PLUGIN_SCOPE_EXAMPLE" -- \
+  melos exec -c 1 --scope="$PLUGIN_EXAMPLE_SCOPE" -- \
     flutter config --enable-windows-desktop && flutter build windows $FLUTTER_COMMAND_FLAGS --debug --target="$TARGET_FILE" --dart-define=CI=true
   exit
 fi
 
-if [ "$ACTION" == "web" ]
+if [ "$JOB_NAME" == "web" ]
 then
-  melos exec -c 1 --scope="$PLUS_PLUGIN_SCOPE_EXAMPLE" -- \
+  melos exec -c 1 --scope="$PLUGIN_EXAMPLE_SCOPE" -- \
     flutter build web $FLUTTER_COMMAND_FLAGS --debug --target="$TARGET_FILE" --dart-define=CI=true
   exit
 fi
