b459fda6d83fc3dce03f79a01945a18b6a6c1d52
Volodymyr Buberenko <v.buberenko@gmail.com>
2023-05-31 11:23:44 +0300

    Another update

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -3,4 +3,5 @@
 BRANCH=$1
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
-echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH && flutter --version
+echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
+&& flutter --version
